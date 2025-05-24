f1e0c455f15ba61a6037617078caea2614a8d7f5
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-24 10:18:55 +0600

    feat: get user endpoint created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
