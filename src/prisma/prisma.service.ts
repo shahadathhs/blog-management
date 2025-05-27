7cef6ca7b8d68244b4a4e5c3493d35ed1442d0af
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-27 10:41:56 +0600

    feat: added prisma logging

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import chalk from 'chalk';
 
 @Injectable()
 export class PrismaService
-  extends PrismaClient
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'error'>
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'error' },
+      ],
+    });
+
+    this.$on('query', (e: Prisma.QueryEvent) => {
+      console.group(chalk.bgBlue.white.bold('📦 Prisma Query Log'));
+      console.log(
+        `${chalk.yellow('🕒 Timestamp:')} ${chalk.white(e.timestamp)}`,
+      );
+      console.log(`${chalk.green('📜 Query:')} ${chalk.white(e.query)}`);
+      console.log(`${chalk.magenta('📦 Params:')} ${chalk.white(e.params)}`);
+      console.log(
+        `${chalk.cyan('⚡ Duration:')} ${chalk.white(`${e.duration} ms`)}`,
+      );
+      console.groupEnd();
+      console.log(chalk.gray('-'.repeat(60)));
+    });
+
+    this.$on('error', (e: Prisma.LogEvent) => {
+      console.group(chalk.bgRed.white.bold('❌ Prisma Error'));
+      console.error(e);
+      console.groupEnd();
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
